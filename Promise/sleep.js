a7407da864a78422e043aa63dee15e4ea4d80c7b
Atik Sahariyar <atiksahariyar88@gmail.com>
2024-01-07 08:30:35 +0600

    added sleep function

---
+++ Promise/sleep.js
@@ -0,0 +1,11 @@
+
+const sleep = async(millis) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, millis);
+    });
+};
+
+
+let t = Date.now();
+sleep(100).then(() => console.log(Date.now() - t));
+
